fbb8e72bba7993f1aa8ec8a0df4050a255647055
Limchasu <mayblue20@naver.com>
2024-05-13 16:29:07 +0900

    first commit

---
+++ src/main/java/com/busanit501/myserver3/myserver3.java
@@ -0,0 +1,30 @@
+package com.busanit501.myserver3;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "myserver3", value = "/myserver3")
+public class myserver3 extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "myserver31";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
